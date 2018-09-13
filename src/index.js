0ce17f1a2478130598bcde99b242ecaab1a1c4ad
Victor Rodzko <vicrodzko@mail.ru>
2018-09-13 11:57:50 +0300

    quadratic equation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+	var arr = equation.split(" ");
+	var a = +arr[0];
+	var b = +(arr[3] + arr[4]);
+	var c = +(arr[7] + arr[8]);
+
+	var D = b*b - 4*a*c;
+
+	var x1 = Math.round((-b + Math.sqrt(D))/(2*a));
+	var x2 = Math.round((-b - Math.sqrt(D))/(2*a));
+
+	var result = ([x1, x2].sort((a,b)=>(a-b)));
+  return result;
 }
