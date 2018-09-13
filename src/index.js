8f324f70ab1528aaf6588de5ed8cc988cc0ffc12
Victor Rodzko <vicrodzko@mail.ru>
2018-09-13 12:00:03 +0300

    quadratic-equation

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 module.exports = function solveEquation(equation) {
-	var arr = equation.split(" ");
-	var a = +arr[0];
-	var b = +(arr[3] + arr[4]);
-	var c = +(arr[7] + arr[8]);
+	let arr = equation.split(" ");
+	let a = +arr[0];
+	let b = +(arr[3] + arr[4]);
+	let c = +(arr[7] + arr[8]);
 
-	var D = b*b - 4*a*c;
+	let D = b*b - 4*a*c;
 
-	var x1 = Math.round((-b + Math.sqrt(D))/(2*a));
-	var x2 = Math.round((-b - Math.sqrt(D))/(2*a));
+	let x1 = Math.round((-b + Math.sqrt(D))/(2*a));
+	let x2 = Math.round((-b - Math.sqrt(D))/(2*a));
 
-	var result = ([x1, x2].sort((a,b)=>(a-b)));
+	let result = ([x1, x2].sort((a,b)=>(a-b)));
   return result;
 }
